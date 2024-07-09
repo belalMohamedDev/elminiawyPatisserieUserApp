323f232a5fb4c3eac0e2bec470c186b69fdd4e8c
belal mohamed <belalagwa0@gmail.com>
2024-07-09 20:06:14 +0200

    add native screen to ios and below andriod 12 and custome splash to andriod above 12

--- lib/feature/splash/presentation/screen/splash_view.dart
+++ lib/feature/splash/presentation/screen/splash_view.dart
@@ -1,100 +1,61 @@
-import 'package:animate_do/animate_do.dart';
-import 'package:elminiawy/core/style/images/asset_manger.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:async';
 
-class SplashView extends StatelessWidget {
+import 'package:elminiawy/core/application/constant_manger.dart';
+import 'package:elminiawy/core/application/di.dart';
+import 'package:elminiawy/core/services/app_storage.dart';
+import 'package:elminiawy/feature/splash/presentation/refactor/splash_body.dart';
+import 'package:flutter/material.dart';
+
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  final AppPreferences _appPreferences = instance<AppPreferences>();
+
+  @override
+  void initState() {
+    Timer(const Duration(seconds: AppConstant.splashDelay), _goNext);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(body: SplashBody());
   }
-}
 
-class SplashBody extends StatelessWidget {
-  const SplashBody({super.key});
+  
 
-  @override
-  Widget build(BuildContext context) {
-    return Swing(
-      infinite: true,
-      // delay: Duration(seconds: 0),
-      duration: const Duration(seconds: 3),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(100.r),
-              child: Image.asset(
-                ImageAsset.splashImage,
-                height: 180.h,
-              ),
-            ),
-            SizedBox(
-              height: 60.h,
-            ),
-            Bounce(
-              infinite: true,
-              delay: const Duration(milliseconds: 750),
-              duration: const Duration(milliseconds: 750),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(100.r),
-                child: Image.asset(
-                  ImageAsset.splashImage,
-                  height: 60.h,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            Bounce(
-              infinite: true,
-              delay: const Duration(milliseconds: 750),
-              duration: const Duration(milliseconds: 750),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(100.r),
-                child: Image.asset(
-                  ImageAsset.splashImage,
-                  height: 30.h,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            Bounce(
-              infinite: true,
-              delay: const Duration(milliseconds: 750),
-              duration: const Duration(milliseconds: 750),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(100.r),
-                child: Image.asset(
-                  ImageAsset.splashImage,
-                  height: 20.h,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 20.h,
-            ),
-            Bounce(
-              infinite: true,
-              delay: const Duration(milliseconds: 750),
-              duration: const Duration(milliseconds: 750),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(100.r),
-                child: Image.asset(
-                  ImageAsset.splashImage,
-                  height: 10.h,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+/////logic to navigation
+  _goNext() async {
+    _appPreferences.isUserLoggedIn().then((isUserLoggedIn) => {
+          if (isUserLoggedIn)
+            {
+              //navigate to main screen
+
+              // Get.offAllNamed(Routes.bottomNavBarRoute)
+            }
+          else
+            {
+              _appPreferences
+                  .isOnBoardingScreenView()
+                  .then((isOnBoardingScreenView) => {
+                        if (isOnBoardingScreenView)
+                          {
+                            //navigate to login screen
+
+                            // Get.offAllNamed(Routes.loginRoute)
+                          }
+                        else
+                          {
+                            //navigate to onBoarding screen
+                            // Get.offAllNamed(Routes.onBoardingRoute)
+                          }
+                      })
+            }
+        });
   }
 }
