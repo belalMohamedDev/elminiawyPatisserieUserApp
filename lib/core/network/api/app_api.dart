83501684eaf6f2846571f1a3144a32cba5dd870e
belal mohamed <belalagwa0@gmail.com>
2024-08-26 01:58:42 +0300

    add UpdateAccountInformationResponse

--- lib/core/network/api/app_api.dart
+++ lib/core/network/api/app_api.dart
@@ -5,8 +5,8 @@ import 'package:elminiawy/feature/address/data/model/response/create_address_res
 
 import 'package:retrofit/retrofit.dart';
 
-import '../../../feature/accountInfo/data/request/update_account_information.dart';
-import '../../../feature/accountInfo/data/response/update_account_information.dart';
+import '../../../feature/accountInfo/data/model/request/update_account_information.dart';
+import '../../../feature/accountInfo/data/model/response/update_account_information.dart';
 import '../../../feature/address/data/model/response/get_store_address_response.dart';
 import '../../../feature/cart/data/model/bodyRequest/add_item.dart';
 import '../../../feature/cart/data/model/response/add_item.dart';
---
+++ lib/feature/accountInfo/data/repository/update_account_information.dart
@@ -0,0 +1,36 @@
+import '../../../../core/network/api/app_api.dart';
+import '../../../../core/network/apiResult/api_reuslt.dart';
+import '../../../../core/network/error_handler/api_error_handler.dart';
+import '../../../../core/network/network_connectivity/connectivity_controller.dart';
+import '../model/request/update_account_information.dart';
+import '../model/response/update_account_information.dart';
+
+abstract class AccountInformationRepository {
+  Future<ApiResult<UpdateAccountInformationResponse>> updateAccountInformation(
+      UpdateAccountInformationRequestBody updateAccountInformationRequestBody);
+}
+
+class AccountInformationRepositoryImplement
+    implements AccountInformationRepository {
+  AccountInformationRepositoryImplement(this._apiService, this._networkInfo);
+  final AppServiceClient _apiService;
+  late final NetworkInfo _networkInfo;
+
+  @override
+  Future<ApiResult<UpdateAccountInformationResponse>> updateAccountInformation(
+      UpdateAccountInformationRequestBody
+          updateAccountInformationRequestBody) async {
+    if (await _networkInfo.isConnected) {
+      try {
+        final response =
+            await _apiService.updateMyData(updateAccountInformationRequestBody);
+        return ApiResult.success(response);
+      } catch (error) {
+        return ApiResult.failure(ErrorHandler.handle(error).apiErrorModel);
+      }
+    } else {
+      //return  internet connection error
+      return ApiResult.failure(DataSource.noInternetConnection.getFailure());
+    }
+  }
+}
