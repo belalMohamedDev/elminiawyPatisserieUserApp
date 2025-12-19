2486367aed9c9284f56fc28c931986fe97d765ca
Belal Mohamed <64976131+belalMohamedDev@users.noreply.github.com>
2025-12-19 20:07:47 +0200

    get all copuns logic

--- lib/feature/admin/coupons/logic/coupons_cubit.dart
+++ lib/feature/admin/coupons/logic/coupons_cubit.dart
@@ -1,9 +1,33 @@
 import 'package:elminiawy/core/common/shared/shared_imports.dart';
 import 'package:elminiawy/feature/admin/coupons/data/model/coupons_response.dart';
+import 'package:elminiawy/feature/admin/coupons/data/repository/coupons_repo.dart';
 
 part 'coupons_state.dart';
 part 'coupons_cubit.freezed.dart';
 
 class CouponsCubit extends Cubit<CouponsState> {
-  CouponsCubit() : super(CouponsState.initial());
+  CouponsCubit(this._couponsRepositoryImplement)
+    : super(CouponsState.initial());
+
+  final CouponsRepositoryImplement _couponsRepositoryImplement;
+
+  List<CouponsData> _coupons = [];
+
+  List<CouponsData> get coupons => _coupons;
+
+  Future<void> getAllCoupons() async {
+    emit(CouponsState.getAllCouponsLoading());
+
+    final response = await _couponsRepositoryImplement.getAllCouponsRepo();
+
+    response.when(
+      success: (dataResponse) {
+        _coupons = dataResponse.data!;
+        emit(CouponsState.getAllCouponsSuccess(dataResponse));
+      },
+      failure: (error) {
+        emit(CouponsState.getAllCouponsError(error));
+      },
+    );
+  }
 }
